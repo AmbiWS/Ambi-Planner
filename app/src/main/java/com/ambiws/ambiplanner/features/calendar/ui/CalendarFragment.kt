0cac2f5e7e91edb5def1727eaca077f1c3655f1c
Mike K. <ambi.ws.studio@gmail.com>
2026-06-16 02:49:21 +0300

    improve calendar ui

--- app/src/main/java/com/ambiws/ambiplanner/features/calendar/ui/CalendarFragment.kt
+++ app/src/main/java/com/ambiws/ambiplanner/features/calendar/ui/CalendarFragment.kt
@@ -3,10 +3,13 @@ package com.ambiws.ambiplanner.features.calendar.ui
 import android.view.View
 import com.ambiws.ambiplanner.base.BaseFragment
 import com.ambiws.ambiplanner.databinding.CalendarDayBinding
+import com.ambiws.ambiplanner.databinding.CalendarMonthHeaderBinding
 import com.ambiws.ambiplanner.databinding.FragmentCalendarBinding
 import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.CalendarMonth
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import com.kizitonwose.calendar.view.MonthDayBinder
+import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
 import com.kizitonwose.calendar.view.ViewContainer
 import java.time.YearMonth
 
@@ -18,25 +21,35 @@ class CalendarFragment : BaseFragment<CalendarViewModel, FragmentCalendarBinding
         super.setupUi()
 
         val monthsToAdd = 100L
-        binding.calendar.dayBinder = object : MonthDayBinder<DayViewContainer> {
-            // Called only when a new container is needed.
-            override fun create(view: View) = DayViewContainer(view)
-
-            // Called every time we need to reuse a container.
-            override fun bind(container: DayViewContainer, data: CalendarDay) {
-                container.textView.text = data.date.dayOfMonth.toString()
+        with(binding) {
+            calendar.dayBinder = object : MonthDayBinder<DayViewContainer> {
+                override fun create(view: View) = DayViewContainer(view)
+                override fun bind(container: DayViewContainer, data: CalendarDay) {
+                    container.textView.text = data.date.dayOfMonth.toString()
+                }
             }
-        }
 
-        val currentMonth = YearMonth.now()
-        val startMonth = currentMonth.minusMonths(monthsToAdd)
-        val endMonth = currentMonth.plusMonths(monthsToAdd)
-        val firstDayOfWeek = firstDayOfWeekFromLocale()
-        binding.calendar.setup(startMonth, endMonth, firstDayOfWeek)
-        binding.calendar.scrollToMonth(currentMonth)
+            calendar.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthHeaderContainer> {
+                override fun create(view: View) = MonthHeaderContainer(view)
+                override fun bind(container: MonthHeaderContainer, data: CalendarMonth) {
+                    container.textView.text = data.yearMonth.month.name
+                }
+            }
+
+            val currentMonth = YearMonth.now()
+            val startMonth = currentMonth.minusMonths(monthsToAdd)
+            val endMonth = currentMonth.plusMonths(monthsToAdd)
+            val firstDayOfWeek = firstDayOfWeekFromLocale()
+            calendar.setup(startMonth, endMonth, firstDayOfWeek)
+            calendar.scrollToMonth(currentMonth)
+        }
     }
 }
 
 class DayViewContainer(view: View) : ViewContainer(view) {
     val textView = CalendarDayBinding.bind(view).calendarDayText
+}
+
+class MonthHeaderContainer(view: View) : ViewContainer(view) {
+    val textView = CalendarMonthHeaderBinding.bind(view).calendarMonthHeaderText
 }
