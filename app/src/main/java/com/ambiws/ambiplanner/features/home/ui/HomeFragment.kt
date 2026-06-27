1cacffcec46c8fe131db766770c149b62aa74486
Mike K. <ambi.ws.studio@gmail.com>
2026-06-27 15:10:23 +0300

    add total routine time

--- app/src/main/java/com/ambiws/ambiplanner/features/home/ui/HomeFragment.kt
+++ app/src/main/java/com/ambiws/ambiplanner/features/home/ui/HomeFragment.kt
@@ -2,12 +2,14 @@ package com.ambiws.ambiplanner.features.home.ui
 
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ambiws.ambiplanner.R
 import com.ambiws.ambiplanner.base.BaseFragment
 import com.ambiws.ambiplanner.base.UiState
 import com.ambiws.ambiplanner.base.list.DefaultListDiffer
 import com.ambiws.ambiplanner.databinding.FragmentHomeBinding
 import com.ambiws.ambiplanner.features.home.ui.list.RoutineAdapterDelegate
 import com.ambiws.ambiplanner.features.home.ui.list.RoutineBaseItemModel
+import com.ambiws.ambiplanner.features.home.ui.list.RoutineItemModel
 import com.ambiws.ambiplanner.utils.extensions.subscribe
 import com.google.android.material.snackbar.Snackbar
 import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
@@ -44,7 +46,23 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>(
         super.setupObservers()
         subscribe(viewModel.routineLiveData) {
             binding.tvEmpty.isVisible = it.isEmpty()
+            binding.tvTotal.isVisible = it.isNotEmpty()
             adapter.items = it
+
+            var totalMinutes = 0
+            it.filterIsInstance<RoutineItemModel>().forEach { item ->
+                item.timeToComplete?.let { timeStr ->
+                    val regex = "(\\d+)h (\\d+)m".toRegex()
+                    val match = regex.find(timeStr)
+                    if (match != null) {
+                        val (hours, minutes) = match.destructured
+                        totalMinutes += hours.toInt() * 60 + minutes.toInt()
+                    }
+                }
+            }
+            val h = totalMinutes / 60
+            val m = totalMinutes % 60
+            binding.tvTotal.text = getString(R.string.total_time_today, h, m)
         }
         subscribe(viewModel.stateLiveEvent) {
             when (it) {
